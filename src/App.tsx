2c877fcc9591fdc56f9c5b70183c0ec9fff4895a
Eregion <wuxize1995@gmail.com>
2023-12-13 15:20:59 -0500

    add redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
-import TipPercentageButton from "./component/TipPercentageButton"
+import React from 'react'
+import styled from "styled-components";
+import { useSelector } from 'react-redux'
+
+import type { RootState } from './app/store'
+import TipPercentageButton from "./component/LeftColumn/TipPercentageButton"
+import AppLayout from './component/AppLayout'
 import GlobalStyle from "./styles/GlobalStyles"
 
+const Wrapper = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100%;
+  width: 100%;
+
+  @media (min-width: 768px) {
+  }
+`;
+
 function App() {
+  const count = useSelector((state: RootState) => state.counter.value)
+
 
   return (
-    <>
-    Hello World
+    // <>
+    // Hello World
+    // <>{count}</>
+    // <TipPercentageButton/>
+    // </>
+    <Wrapper>
     <GlobalStyle/>
-    <TipPercentageButton/>
-    </>
+    <AppLayout/>
+    </Wrapper>
+
   )
 }
 
